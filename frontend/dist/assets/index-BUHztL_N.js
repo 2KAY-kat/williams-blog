d2cd39c72306d1659d39751d529788bc46b4f692
2KAY-kat <dambwedesigns@gmail.com>
2025-11-24 14:03:45 +0200

    dist folder ready

---
+++ frontend/dist/assets/index-BUHztL_N.js
@@ -0,0 +1,11 @@
+function a(){return`
+        <div class="main-header">
+            <a href="/" class="logo"><span>Williams</span> Kaphika</a>
+            <a href="/" class="logo-mobile logo"><span>W</span>K</a>
+            <nav class="main-nav">
+                <a href="/admin/dashboard" class="cta-link icon" id="add-article-link" title="Add New" Article"><i class="fa fa-pen"></i></a>
+                <a href="/auth/signup" class="cta-link icon"><i class="fa fa-sign-in"></i></a>
+                <a href="#" class="cta-link subscribe-btn">Subscribe</a>
+            </nav>
+        </div>
+        `}document.querySelector("header").innerHTML=a();
