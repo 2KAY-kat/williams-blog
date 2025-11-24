e9d0066101cc2c36ba42af381cbc38359626e724
2KAY-kat <dambwedesigns@gmail.com>
2025-11-24 14:19:23 +0200

    file extetion

--- frontend/dist/assets/index-CsxmzJuP.js
+++ frontend/dist/assets/index-CsxmzJuP.js
@@ -3,8 +3,8 @@ function a(){return`
             <a href="/" class="logo"><span>Williams</span> Kaphika</a>
             <a href="/" class="logo-mobile logo"><span>W</span>K</a>
             <nav class="main-nav">
-                <a href="/admin/dashboard" class="cta-link icon" id="add-article-link" title="Add New" Article"><i class="fa fa-pen"></i></a>
-                <a href="/auth/signup" class="cta-link icon"><i class="fa fa-sign-in"></i></a>
+                <a href="/admin/dashboard.html" class="cta-link icon" id="add-article-link" title="Add New" Article"><i class="fa fa-pen"></i></a>
+                <a href="/auth/signup.html" class="cta-link icon"><i class="fa fa-sign-in"></i></a>
                 <a href="#" class="cta-link subscribe-btn">Subscribe</a>
             </nav>
         </div>
--- frontend/src/js/components/header.js
+++ frontend/src/js/components/header.js
@@ -5,8 +5,8 @@ export function headerComponentHTML() {
             <a href="/" class="logo"><span>Williams</span> Kaphika</a>
             <a href="/" class="logo-mobile logo"><span>W</span>K</a>
             <nav class="main-nav">
-                <a href="/admin/dashboard" class="cta-link icon" id="add-article-link" title="Add New" Article"><i class="fa fa-pen"></i></a>
-                <a href="/auth/signup" class="cta-link icon"><i class="fa fa-sign-in"></i></a>
+                <a href="/admin/dashboard.html" class="cta-link icon" id="add-article-link" title="Add New" Article"><i class="fa fa-pen"></i></a>
+                <a href="/auth/signup.html" class="cta-link icon"><i class="fa fa-sign-in"></i></a>
                 <a href="#" class="cta-link subscribe-btn">Subscribe</a>
             </nav>
         </div>
